29f193f4229bb3ad7c7c6ac9b16fb004fcbc3e30
Pedro Afonso <ycomicas@gmail.com>
2025-09-05 18:09:27 -0300

    material_radiotivo_resolvido

--- material_radioativo.py
+++ material_radioativo.py
@@ -1,1 +1,11 @@
-# Sua solução aqui
+massa = float(input("Digite a massa inicial: "))
+tempo = 0  
+while massa > 0.5 or massa == 0.5:
+    massa = massa / 2
+    tempo = tempo + 50
+massa_final = massa
+h = tempo // 3600
+m = (tempo % 3600) // 60
+s = tempo % 60
+print(f"Massa final: {massa_final:.2f} g")
+print(f"{int(h)}h {int(m)}m {int(s)}s")
